e4ad99d8fab9285fafd1fe8734f542a67782f176
Rai Barbosa de Freitas <rai082020@gmail.com>
2023-09-06 10:49:18 -0300

    controle-fluxo/src

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,11 +2,15 @@ public class ExemploForArray {
 
     //Arrays se iniciam com 0!
 
-    public static void main(String[] args) {
+         public static void main(String[] args) {
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
-	for (int x=0; x<alunos.length; x++) {
-		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+
+
+	for (String aluno : alunos){
+		    System.out.println("Nome do aluno e:" + aluno);
 	}
     }
+
+    
 }
